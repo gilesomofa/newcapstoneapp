b874f0d81ade5a6fe0b3c7be57feddc5cbf3eacf
Giles Smith <clintphillips@Giles-MacBook-Pro.local>
2020-05-13 16:58:52 -0500

    Revert "adding new changs to reducers" making sure all is organized "making sure all is organized" ls git status
    This reverts commit 789ec54eec25deaf64575375aed0601816a923a0.

--- src/reducers/studentReducer.js
+++ src/reducers/studentReducer.js
@@ -1,33 +1,21 @@
-import { FETCH_STUDENTS, NEW_STUDENT, NEW_BEHAVIOR } from "../actions/types";
+import { FETCH_STUDENTS, NEW_STUDENT } from '../actions/types';
 
 const initialState = {
   items: [],
-  item: {},
-  behavior: [],
-  newBehavior: [],
+  item: {}
 };
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_STUDENTS:
       return {
         ...state,
-        items: action.payload,
+        items: action.payload
       };
     case NEW_STUDENT:
       return {
         ...state,
-        item: action.payload,
-      };
-    case CREATE_BEHAVIOR:
-      return {
-        ...state,
-        behavior: action.payload,
-      };
-    case NEW_BEHAVIOR:
-      return {
-        ...state,
-        newBehavior: action.payload,
+        item: action.payload
       };
     default:
       return state;
