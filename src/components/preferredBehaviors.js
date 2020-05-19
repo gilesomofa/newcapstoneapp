4403acb112b24ea9b8d3d07f7aba6881565460d9
Giles Smith <clintphillips@Giles-MacBook-Pro.local>
2020-05-18 23:06:55 -0500

    add dynamic reuseable  CheckBox component

--- src/components/preferredBehaviors.js
+++ src/components/preferredBehaviors.js
@@ -1,29 +1,58 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-// import { getPreferredBehaviors } from "../actions/behaviorActions";
+import BehaviorCheckbox from "./BehaviorCheckbox";
 
 const preferredBehaviors = require("../behaviorState/preferredBehaviors.json");
-console.log({ preferredBehaviors });
-class PreferredBehaviors extends React.Component {
-  componentWillMount = () => this.props.preferredBehaviors;
 
+class PreferredBehaviors extends React.Component {
+  constructor(props) {
+    super(props);
+    // componentWillMount = () => this.props.preferredBehaviors;
+    this.state = {
+      checkedItems: new Map(),
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.isChecked;
+    this.setState((prevState) => ({
+      checkedItems: prevState.checkedItems.set(item, isChecked),
+    }));
+  }
+  //   render() {
+  //     const preferredBehaviorItems = this.props.preferredBehaviors.map(
+  //       (behavior) => (
+  //         <div key={behavior.type.id}>
+  //           <h3>
+  //             {behavior.type.name} {behavior.name}
+  //           </h3>
+  //         </div>
+  //       )
+  //     );
+  //     return (
+  //       <div className="preferredBehaviorsItems">
+  //         {<h1>Preferred Behaviors</h1>}
+  //         <form input="checkbox">{preferredBehaviorItems} </form>
+  //       </div>
+  //     );
+  //   }
   render() {
-    const preferredBehaviorItems = this.props.preferredBehaviors.map(
-      (behavior) => (
-        <div key={behavior.type.id}>
-          <h3>
-            {behavior.type.name} {behavior.name}
-          </h3>
-        </div>
-      )
-    );
     return (
-      <div>
-        {" "}
-        {<h1>Preferred Behaviors</h1>}
-        {preferredBehaviorItems}{" "}
-      </div>
+      <React.Fragment>
+        {preferredBehaviors.map((item) => (
+          <label key={item.key}>
+            {item.name}
+            <BehaviorCheckbox
+              name={item.name}
+              checked={this.state.checkedItems.get(item.name)}
+              onChange={this.handleChange}
+            />
+          </label>
+        ))}
+      </React.Fragment>
     );
   }
 }
